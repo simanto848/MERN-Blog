989b149a4f4098a0445898a2f937acfaa58125c0
Simanto Hasan <simanto848@gmail.com>
2024-01-23 00:15:08 +0600

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import db from "../db.js";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -51,5 +52,56 @@ export const signup = async (req, res, next) => {
     });
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const sql = "SELECT * FROM users WHERE email = ?";
+
+    db.query(sql, [email], async (err, data) => {
+      if (err) {
+        return next(errorHandler(500, "Internal server error"));
+      }
+      if (!data.length)
+        return next(errorHandler(400, "Email or password is incorrect"));
+
+      const isPasswordCorrect = await bcrypt.compare(
+        password,
+        data[0].password
+      );
+
+      if (!isPasswordCorrect)
+        return next(errorHandler(400, "Email or password is incorrect"));
+
+      const token = jwt.sign(
+        {
+          id: data[0].id,
+          username: data[0].username,
+          email: data[0].email,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      delete data[0].password;
+
+      return res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(data[0]);
+    });
+  } catch (error) {
+    return next(error);
   }
 };
